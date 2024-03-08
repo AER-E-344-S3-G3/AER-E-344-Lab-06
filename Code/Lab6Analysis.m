bb9c1eccf79bb3a466cac01a01ac0a8fe30dbf44
gmartinp03 <gmp@iastate.edu>
2024-03-08 16:32:20 -0600

    Fix integral approximation loop for C_D

--- Code/Lab6Analysis.m
+++ Code/Lab6Analysis.m
@@ -6,21 +6,21 @@ data_dir = "./Data/";
 % Viscosity calculated at 21.2°C
 % https://www.engineeringtoolbox.com/air-absolute-kinematic-viscosity-d_601
 % .html
-mu_air = 18.18e-6; % [Pa*s]
+mu_air = 18.18e-6; % [Pa*s] 
 rho_air = 1.225; % [kg/m^3]
-V_inf = 19.4; % [m/s} 
-K = 1.1;
-chord = 0.101; % [m] 
-d_taps = 0.002; % [m] distance between taps
+V_inf = 19.4; % [m/s] V
+K = 1.1; % Calibration Constant
+chord = 0.101; % [m] Chord of airfoil
+d_taps = 0.002; % [m] Cistance between taps
 
 %% Part 1 Airfoil Wake 
 aoa = [0 4 6 8 10 12];
 n_aoa = length(aoa);
 wake_files = dir(data_dir + '*deg.csv');
-range = cat(2,(2:17),(35: 50),(68: 81)); % Columns containing pressure values from raw data
+% Columns containing pressure values from raw data
+range = cat(2,(2:17),(35: 50),(68: 81));
 n_taps = length(range); % number of taps
 position = (1:n_taps); % position of taps
-
 PA_idx = 82;
 PE_idx = 83;
 C_p = zeros(n_aoa, n_taps); % Initialize matrix for pressure data
@@ -28,20 +28,23 @@ U_p = zeros(n_aoa, n_taps); % Initialize matrix for velocity data
 C_D = zeros(1,n_aoa);
 U_inf = zeros(1,n_aoa);
 for a = 1:n_aoa % Iterate through files
-    temp = mean(readmatrix(data_dir+wake_files(a).name)); % Read file & find average of columns
+    % Read file & find average of columns
+    temp = mean(readmatrix(data_dir+wake_files(a).name));
     count = 1;
     PA = temp(PA_idx);
     PE = temp(PE_idx);
-    q = K *(PA - PE);
-    U_inf(a) = sqrt(2 * q / rho_air );
+    q = K *(PA - PE); % Dynamic Pressure
+    U_inf(a) = sqrt(2 * q / rho_air ); % Freestream velocity
     for i = 1 : width(temp) % Iterate through data
         if ismember(i,range) % Take columns with pressure data
             C_p(a, count) = (temp(i)-PE) / q ; % Cp at each tap
-            U_p(a,count) = sqrt((temp(i) - PE) * 2 / rho_air); % Velocity at each tap
+            % Velocity at each tap
+            U_p(a,count) = sqrt((temp(i) - PE) * 2 / rho_air);
             count = count + 1;
 
         end 
     end
+    % C_P Graphs
     figure(a)
     plot(position, C_p(a,:))
     fontname("Times New Roman");
@@ -54,14 +57,14 @@ for a = 1:n_aoa % Iterate through files
     % saveas(gcf, figure_dir + title_str + ".svg");
 
 % midpoint rule for approx integral
-    for i = 1:range-1
+    for i = 1:n_taps-1
         f1 = U_p(a, i)/U_inf(a) * (1 - U_p(a, i)/U_inf(a));
         f2 = U_p(a, i+1)/U_inf(a) * (1 - U_p(a, i+1)/U_inf(a));
         C_D(a) = C_D(a) + d_taps*((f1 + f2)/2);
     end
     C_D(a) = C_D(a) * 2 / chord;
 end
-
+% C_D Graph
 figure(a + 1)
 plot(aoa, C_D)
     fontname("Times New Roman");
@@ -71,6 +74,3 @@ plot(aoa, C_D)
     xlabel("Angle of Attack [^{\circ}]");
     ylabel("C_D");
     grid on;
-
-%% Part 2 Anemometer Calibration
-%TODO
